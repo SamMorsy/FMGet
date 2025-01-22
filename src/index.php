607493db1bd57e89f57103bc95f247e8251561bd
Sam Morsy <info@fmget.com>
2025-01-22 13:36:56 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
 <?php
 
-// Starting here
+// State for index
+//Not configured [ without config.json ]
+//Not configured [ without server details ]
+//Not configured [ without form details ]
+//Temporary unavailable [ status variable is offline ]
+//Available [ Render form ]
+//Critical error [ failed to submit ]
+//Submit Message [ All tests passed ]
